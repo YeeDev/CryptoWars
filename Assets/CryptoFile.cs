65e38417d38d315b31cfb26a48c968d0191f4a5c
Yee <yee.gamedev@gmail.com>
2022-10-02 12:27:31 -0500

    Floor Takes Damage

---
+++ Assets/CryptoFile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CryptoFile : MonoBehaviour
+{
+    [SerializeField] int health = 5;
+    [SerializeField] float loweredHeightAmount = 0.5f;
+
+    //TODO Refactor this, POSSNewName TakeDamage
+    public void ReduceHealth(int damage)
+    {
+        health -= damage;
+
+        Vector3 loweredPosition = transform.position;
+        loweredPosition.y -= loweredHeightAmount;
+        transform.position = loweredPosition;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
