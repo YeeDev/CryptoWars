b8a7b4bb88d3fdd86c5ebc252cdfa92621680c12
Yee <yee.gamedev@gmail.com>
2022-10-02 11:43:06 -0500

    Mech now aims with mouse

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -6,6 +6,9 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0.2f;
     [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] Transform tankHead = null;
+    [SerializeField] float mousePrecision = 0.1f;
+    [SerializeField] Transform cannon = null;
 
     Rigidbody rb;
 
@@ -22,6 +25,12 @@ public class Mover : MonoBehaviour
         rb.MovePosition(transform.position + step);
 
         Quaternion rotateTo = Quaternion.Euler(transform.rotation.eulerAngles + (transform.up * Input.GetAxis("Horizontal")));
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateTo, rotationSpeed); 
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateTo, rotationSpeed);
+
+        Vector3 mouseCursorPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+    -transform.forward.z - -Camera.main.transform.forward.z + mousePrecision));
+        cannon.LookAt(mouseCursorPoint, Vector3.up);
+        mouseCursorPoint.y = tankHead.position.y;
+        tankHead.LookAt(mouseCursorPoint, Vector3.up);
     }
 }
