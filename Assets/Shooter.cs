1fb67e765f9359e8b6624bfd740ab9add74eeef7
Yee <yee.gamedev@gmail.com>
2022-10-02 19:20:52 -0500

    Refactor: Shooter Script, removed shooting behaviour from Mover

---
+++ Assets/Shooter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace CryptoWars.Attacks
+{
+    public class Shooter : MonoBehaviour
+    {
+        [SerializeField] GameObject bulletPrefab = null;
+        [SerializeField] Transform muzzle = null;
+        [SerializeField] float bulletSpeed = 5f; //TODO refactor to weapon
+
+        //TODO BulletPooler
+        //Needs to create a list of weapons bullets based on type
+        public void Shoot()
+        {
+            Rigidbody bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation).GetComponent<Rigidbody>();
+            bullet.velocity = bullet.transform.forward * bulletSpeed;
+        }
+    }
+}
