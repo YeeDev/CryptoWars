24effec13f9fe75740a1404a8a8f2134bfab100f
Yee <yee.gamedev@gmail.com>
2022-10-06 15:08:40 -0500

    Test To flip transform when in ceiling = FAILURE...

--- Assets/PhysicsApplier.cs
+++ Assets/PhysicsApplier.cs
@@ -22,10 +22,20 @@ namespace CryptoWars.CustomPhysics
 
         public void ApplyGravity() { rb.AddForce(Vector3.up * gravityForce); }
 
-        public void CheckIfInvertGravity()
+        public void CheckIfInvertGravity() //TODO handle this in animation Child objects
         {
-            if (transform.position.y > gravityThreshold && gravityForce < 0) { gravityForce = -gravityForce; }
-            else if (transform.position.y < gravityThreshold && gravityForce > 0) { gravityForce *= -1; }
+            if (transform.position.y > gravityThreshold && gravityForce < 0)
+            {
+                gravityForce *= -1;
+                Vector3 currentRotation = transform.rotation.eulerAngles;
+                currentRotation += Vector3.forward * 180;
+                transform.rotation = Quaternion.Euler(currentRotation);
+            }
+            else if (transform.position.y < gravityThreshold && gravityForce > 0) { gravityForce *= -1;
+                Vector3 currentRotation = transform.rotation.eulerAngles;
+                currentRotation += Vector3.forward * 180;
+                transform.rotation = Quaternion.Euler(currentRotation);
+            }
         }
     }
 }
