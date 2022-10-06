930d2572930d04497cd734dcca37fa62a9d2aa57
Yee <yee.gamedev@gmail.com>
2022-10-06 12:01:11 -0500

    Crypto File now Adds currency

--- Assets/CryptoFile.cs
+++ Assets/CryptoFile.cs
@@ -1,11 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CryptoFile : MonoBehaviour
 {
+    [SerializeField] int currencyValue = 5;
     [SerializeField] int health = 5;
     [SerializeField] float loweredHeightAmount = 0.5f;
+
+    UIUpdater uIUpdater;
+
+    private void Awake()
+    {
+        uIUpdater = FindObjectOfType<UIUpdater>();
+    }
 
     //TODO Refactor this, POSSNewName TakeDamage
     public void ReduceHealth(int damage)
@@ -18,6 +24,7 @@ public class CryptoFile : MonoBehaviour
 
         if (health <= 0)
         {
+            uIUpdater.UpdateCurrency(currencyValue);
             Destroy(gameObject);
         }
     }
