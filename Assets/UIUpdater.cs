086f56974d8364842c1af135d346779191694c50
Yee <yee.gamedev@gmail.com>
2022-10-06 12:00:44 -0500

    UIUpdate Script Created

---
+++ Assets/UIUpdater.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIUpdater : MonoBehaviour
+{
+    [Tooltip("For debuggin purposes")]
+    [SerializeField] int currency = 0;  //TODO refactor this to another script
+    [SerializeField] Text currencyText = null;
+    [SerializeField] RectTransform fuelBar = null;
+
+    float fuelBarMaxXSize;
+
+    private void Awake() { fuelBarMaxXSize = fuelBar.sizeDelta.x; }
+
+    public void UpdateCurrency(int currencyToAdd)
+    {
+        currency += currencyToAdd;
+        currencyText.text = $"Currency: {currency}";
+    }
+
+    public void UpdateFuelBar(float fuelRemaining, float maxFuel)
+    {
+        Vector2 fuelBarSize = fuelBar.sizeDelta;
+        fuelBarSize.x = (fuelRemaining * fuelBarMaxXSize) / maxFuel;
+        fuelBar.sizeDelta = fuelBarSize;
+    }
+}
