b2da27cadbf87294604688b75b094f4ae595a65f
Yee <yee.gamedev@gmail.com>
2022-10-06 11:02:15 -0500

    Follower Script Created

---
+++ Assets/Follower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CryptoWars.Movement;
+
+public class Follower : MonoBehaviour
+{
+    //TODO Set this variable on awake, not in inspector
+    [SerializeField] Transform player = null;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.position;
+    }
+}
