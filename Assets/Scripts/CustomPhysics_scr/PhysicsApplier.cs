acfd2b635f96c2a83b81be711a77e61dc76d7989
Yee <yee.gamedev@gmail.com>
2022-10-06 20:45:29 -0500

    PhysicsApplier now Flips The Player

--- Assets/Scripts/CustomPhysics_scr/PhysicsApplier.cs
+++ Assets/Scripts/CustomPhysics_scr/PhysicsApplier.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using CryptoWars.Animations;
 
 namespace CryptoWars.CustomPhysics
 {
@@ -8,6 +9,7 @@ namespace CryptoWars.CustomPhysics
 
         float gravityThreshold;
         Rigidbody rb;
+        Animater anm;
 
         public float GravityDirection { get => Mathf.Sign(gravityForce) * -1; }
         public Rigidbody RB { get => rb; }
@@ -16,14 +18,21 @@ namespace CryptoWars.CustomPhysics
         {
             gravityThreshold = GameObject.FindGameObjectWithTag("Terrain").transform.position.y;
             rb = GetComponent<Rigidbody>();
+            anm = GetComponent<Animater>();
         }
 
         public void ApplyGravity() { rb.AddForce(Vector3.up * gravityForce); }
 
         public void CheckIfInvertGravity() //TODO handle this in animation Child objects
         {
-            if (transform.position.y > gravityThreshold && gravityForce < 0) { gravityForce *= -1; }
-            else if (transform.position.y < gravityThreshold && gravityForce > 0) { gravityForce *= -1; }
+            if (transform.position.y > gravityThreshold && gravityForce < 0) { FlipZAxis(); }
+            else if (transform.position.y < gravityThreshold && gravityForce > 0) { FlipZAxis(); }
+        }
+
+        public void FlipZAxis()
+        {
+            gravityForce *= -1;
+            anm.FlipZAxis();
         }
     }
 }
