271d8b55d1b0a488a124b22e96e42eae0d12c09e
Yee <yee.gamedev@gmail.com>
2022-10-06 20:44:48 -0500

    Animater Script Created

---
+++ Assets/Scripts/Animations_scr/Animater.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace CryptoWars.Animations
+{
+    public class Animater : MonoBehaviour
+    {
+        [SerializeField] string flipZParameter = "IsGravityUpwards";
+
+        Animator anm;
+
+        private void Awake() { anm = GetComponent<Animator>(); }
+
+        public void FlipZAxis() { anm.SetBool(flipZParameter, !anm.GetBool(flipZParameter)); } 
+    }
+}
