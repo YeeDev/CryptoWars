5848b3912d52bf385a947f179ac31b03824cf839
Yee <yee.gamedev@gmail.com>
2022-10-07 17:58:26 -0500

    Bullet Is calculated in both client and server

--- Assets/Scripts/AttackTypes_scr/Bullet.cs
+++ Assets/Scripts/AttackTypes_scr/Bullet.cs
@@ -1,19 +1,31 @@
+using Mirror;
 using UnityEngine;
 using CryptoWars.Currency;
 
 namespace CryptoWars.AttackTypes
 {
-    public class Bullet : MonoBehaviour
+    public class Bullet : NetworkBehaviour
     {
         [SerializeField] int damage = 1;
+        [SerializeField] float bulletspeed = 40f;
+        [SerializeField] Rigidbody rb = null;
 
+        private void Start()
+        {
+            rb.velocity = transform.forward * bulletspeed;
+        }
+
+        [ServerCallback]
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Currency"))
             {
                 other.GetComponent<CryptoFile>().ReduceHealth(damage);
-                Destroy(gameObject);
+                DestroySelf();
             }
         }
+
+        [Server]
+        void DestroySelf() => NetworkServer.Destroy(gameObject);
     }
 }
