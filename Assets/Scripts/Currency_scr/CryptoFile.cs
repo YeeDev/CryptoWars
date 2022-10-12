a3b848ddd825cee1884706792bd7de7cea319507
Yee <yee.gamedev@gmail.com>
2022-10-11 19:11:58 -0500

    CyrptoFiles Change color when hit

--- Assets/Scripts/Currency_scr/CryptoFile.cs
+++ Assets/Scripts/Currency_scr/CryptoFile.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using CryptoWars.UI;
+using Mirror;
 
 namespace CryptoWars.Currency
 {
-    public class CryptoFile : MonoBehaviour
+    public class CryptoFile : NetworkBehaviour
     {
         [SerializeField] int currencyValue = 5;
-        [SerializeField] int health = 5;
+        [SerializeField] int maxHealth = 5;
         [SerializeField] float heightChange = 0.5f;
+        [SerializeField] Color initialColor = Color.blue;
+        [SerializeField] Color finalColor = Color.red;
 
+        float health;
         float moveDirectionThreshold;
         UIUpdater uIUpdater;
+        Material material;
+
+        [SyncVar(hook = nameof(SetColor))] Color damageColor = Color.black;
+
+        private void SetColor(Color oldColor, Color newColor) => material.color = newColor;
 
         private void Awake()
         {
             uIUpdater = FindObjectOfType<UIUpdater>();
             moveDirectionThreshold = GameObject.FindGameObjectWithTag("Terrain").transform.position.y;
+            material = GetComponent<MeshRenderer>().material;
+
+            health = maxHealth;
         }
 
         //TODO Refactor this, POSSNewName TakeDamage
@@ -26,6 +38,7 @@ namespace CryptoWars.Currency
             Vector3 loweredPosition = transform.position;
             loweredPosition.y += transform.position.y < moveDirectionThreshold ? -heightChange : heightChange;
             transform.position = loweredPosition;
+            damageColor = Color.Lerp(finalColor, initialColor, health / maxHealth);
 
             if (health <= 0)
             {
